c2f8823d12690d4cbd27adcbb4fbe9d10d99e7fd
ᴛᴀᴅᴀɴᴏ <skirichenko477@gmail.com>
2026-01-19 14:39:08 +0200

    Refactor InfinityX build script for improved efficiency
    Updated the InfinityX build script to remove deprecated commands and streamline the initialization process for device trees and vendor repositories.

--- infi.sh
+++ infi.sh
@@ -4,90 +4,56 @@
 #  InfinityX Build Script for Poco F6 Pro (vermeer)
 # ==========================================================
 
-# Переменные
-DEVICE="vermeer"
-COMMON="sm8550-common"
-ROM_BRANCH="16" # Android 16 (InfinityX)
-LOS_BRANCH="lineage-23.1" 
 
-# --- Очистка старых манифестов ---
+
+# --- Remove old local manifests ---
 rm -rf .repo/local_manifests
 rm -rf .repo/manifests
 rm -rf .repo/manifest.xml
 
-# --- Инициализация и Синхронизация ---
-repo init -u https://github.com/Mi-Apollo/infinity_manifest.git -b $ROM_BRANCH --git-lfs
 
-if [ -f /opt/crave/resync.sh ]; then
-    /opt/crave/resync.sh
-else
-    repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-fi
-
-# --- Клонирование Device Trees ---
-rm -rf device/xiaomi/$DEVICE device/xiaomi/$COMMON
-git clone https://github.com/LineageOS/android_device_xiaomi_$DEVICE -b $LOS_BRANCH device/xiaomi/$DEVICE
-git clone https://github.com/LineageOS/android_device_xiaomi_$COMMON -b $LOS_BRANCH device/xiaomi/$COMMON
-
-# --- Клонирование Vendor ---
-rm -rf vendor/xiaomi/$DEVICE vendor/xiaomi/$COMMON
-git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_$DEVICE -b $LOS_BRANCH vendor/xiaomi/$DEVICE
-git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_$COMMON -b $LOS_BRANCH vendor/xiaomi/$COMMON
-
-# --- Клонирование Kernel (SM8550) ---
-rm -rf kernel/xiaomi/sm8550 kernel/xiaomi/sm8550-devicetrees kernel/xiaomi/sm8550-modules
-git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8550 -b $LOS_BRANCH kernel/xiaomi/sm8550
-git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8550-devicetrees -b $LOS_BRANCH kernel/xiaomi/sm8550-devicetrees
-git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8550-modules -b $LOS_BRANCH kernel/xiaomi/sm8550-modules
-
-# --- Дополнительные репозитории (Фиксы) ---
-rm -rf hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b $LOS_BRANCH hardware/xiaomi
-
-# Фикс отсутствующего модуля Calendar (как в твоем логе)
-rm -rf packages/apps/Calendar
-git clone https://github.com/LineageOS/android_packages_apps_Calendar -b $LOS_BRANCH packages/apps/Calendar
-
+# --- Remove Device Settings --- (Reason: It Will fail sync when we re run this script)
 rm -rf packages/resources/devicesettings
-git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b $LOS_BRANCH packages/resources/devicesettings
 
-# --- Адаптация дерева Lineage под InfinityX ---
-# Это решит ошибку "Cannot locate config makefile"
-cd device/xiaomi/$DEVICE
-if [ -f lineage_$DEVICE.mk ]; then
-    cp lineage_$DEVICE.mk infinity_$DEVICE.mk
-    sed -i "s/lineage_$DEVICE/infinity_$DEVICE/g" infinity_$DEVICE.mk
-    sed -i "s/lineage/infinity/g" infinity_$DEVICE.mk
-    echo "Successfully converted lineage_$DEVICE.mk to infinity_$DEVICE.mk"
-fi
-cd ../../../
+# --- Init ROM repo ---
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle && \
 
-# --- Подготовка окружения ---
-. build/envsetup.sh
-export INFINITY_MAINTAINER="tadano"
+# --- Sync ROM ---
+#/opt/crave/resync.sh && \
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \
 
-# ==========================================================
-#  SBORKA: Vanilla
-# ==========================================================
-echo "===== STARTING VANILLA BUILD ====="
-export WITH_GAPPS=false
-lunch infinity_$DEVICE-user || lunch lineage_$DEVICE-user
-make installclean
-m bacon
+# --- Clone Device Tree ---
+rm -rf device/xiaomi
+git clone https://github.com/Tadassha/android_device_xiaomi_vermeer -b lineage-23.0 device/xiaomi/vermeer && \
 
-# Перемещаем билд
-mkdir -p out/final_output/vanilla
-mv out/target/product/$DEVICE/InfinityX*.zip out/final_output/vanilla/ 2>/dev/null
+# --- Clone Hardware Tree ---
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi && \
+#git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi && \
 
-# ==========================================================
-#  SBORKA: GApps
-# ==========================================================
-echo "===== STARTING GAPPS BUILD ====="
-export WITH_GAPPS=true
-make installclean 
-m bacon
+# --- Clone Kernel Tree ---
+rm -rf kernel/xiaomi/sm8550 kernel/xiaomi/sm8550-devicetrees kernel/xiaomi/sm8550-modules
+git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8550 -b lineage-23.0 kernel/xiaomi/sm8550
+git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8550-devicetrees -b lineage-23.0 kernel/xiaomi/sm8550-devicetrees
+git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8550-modules -b lineage-23.0 kernel/xiaomi/sm8550-modules
 
-mkdir -p out/final_output/gapps
-mv out/target/product/$DEVICE/InfinityX*.zip out/final_output/gapps/ 2>/dev/null
+# --- Clone Vendor Tree ---
+rm -rf vendor/xiaomi
+git clone https://github.com/xiaomi-8550/proprietary_vendor_xiaomi_vermeer -b beryl vendor/xiaomi/vermeer && \
 
-echo "===== ALL BUILDS FINISHED! CHECK out/final_output ====="
+# --- Device Settings ---
+rm -rf packages/resources/devicesettings
+https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-23.0 packages/resources/devicesettings && \
+
+# =============================
+#  Build: Vanilla → Gapps
+# =============================
+
+# --- Vanilla Build ---
+echo "===== Starting Vanilla Build ====="
+. build/envsetup.sh && \
+breakfast apollo user && \
+make installclean && \
+mka bacon
+
+echo "===== All builds completed successfully! ====="
